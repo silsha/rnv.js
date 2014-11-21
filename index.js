072a0c5f34a69871f2c7ae19caca15b05866afef
Ralph Greschner <ralph.greschner@gmail.com>
2014-11-21 18:09:36 +0100

    Source formatting. Callbacks are returning data as JSON objects now. Factory method for client. Proxy & useragent support.

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
-var request     = require('request');
+var request = require('request');
 var querystring = require('querystring');
 
-var apiurl      = 'http://rnv.the-agent-factory.de:8080/easygo2/rest/regions/rnv/modules/';
+/**
+ * Default user agent for HTTP requests.
+ */
+const DEFAULT_UA = 'easy.GO Client Android v1.2.1 (Mozilla/5.0 (Linux; Android 4.4.4; Nexus 4 Build/KTU84Q) ' +
+    'AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36)';
 
+/**
+ * Default API endpoint.
+ */
+const API_URL = 'http://rnv.the-agent-factory.de:8080/easygo2/rest/regions/rnv/modules/';
 
-var rnv = function () {
+/**
+ * Default client config.
+ */
+const DEFAULT_CONFIG = {
+    userAgent: "default"
+};
+
+var rnv = function (opts) {
+
+    if (!opts) {
+        opts = DEFAULT_CONFIG;
+    }
+
+    var proxy = opts.proxy;
+    var userAgent = opts.userAgent;
+
+    if ("default" == userAgent) {
+        userAgent = DEFAULT_UA;
+    }
+
     this.stations = function (callback) {
         var data = {};
         data.path = 'stations/packages/1';
-        getResult(data, function(res){
+        getResult(data, function (res) {
             callback(res);
         });
     }
 
-    this.stationmonitor = function (query, callback) {
+    this.stationMonitor = function (query, callback) {
         var data = {};
         data.query = query;
         data.path = "stationmonitor/element";
-        getResult(data, function(res){
+        getResult(data, function (res) {
             callback(res);
         })
     }
+
+    // For compatiblity reasons.
+    this.stationmonitor = this.stationMonitor;
 
     this.lines = function (query, callback) {
         var data = {};
         data.query = query;
         data.path = "lines";
-        getResult(data, function(res){
+        getResult(data, function (res) {
             callback(res);
         })
     }
@@ -34,7 +64,7 @@ var rnv = function () {
     this.ticker = function (callback) {
         var data = {};
         data.path = 'ticker/'
-        getResult(data, function(res){
+        getResult(data, function (res) {
             callback(res);
         })
 
@@ -43,7 +73,7 @@ var rnv = function () {
     this.tickerCount = function (callback) {
         var data = {};
         data.path = 'ticker/numberOfNewEntries/0'
-        getResult(data, function(res){
+        getResult(data, function (res) {
             callback(res);
         })
 
@@ -52,7 +82,7 @@ var rnv = function () {
     this.news = function (callback) {
         var data = {};
         data.path = 'news/'
-        getResult(data, function(res){
+        getResult(data, function (res) {
             callback(res);
         })
 
@@ -61,21 +91,25 @@ var rnv = function () {
     this.newsCount = function (callback) {
         var data = {};
         data.path = 'news/numberOfNewEntries/0'
-        getResult(data, function(res){
+        getResult(data, function (res) {
             callback(res);
         })
 
     }
 
-    function getResult (data, callback) {
+    function getResult(data, callback) {
         var query = "";
-        if(data.query)
+        if (data.query)
             query = "?" + querystring.stringify(data.query);
 
-        request.get(apiurl + data.path + query, function (err, res, body) {
+        var url = API_URL + data.path + query;
+        request.get({url: url, json: true, proxy: proxy}, function (err, res, body) {
             callback(body);
         });
     }
 }
 
 module.exports = rnv;
+module.exports.createClient = function (opts) {
+    return new rnv(opts);
+}
